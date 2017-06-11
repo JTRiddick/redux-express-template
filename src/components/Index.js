1ae1566efd8d2f506546504a69f46ae94cabe068
Tatrakrad <riddick.taylor@gmail.com>
2017-06-11 17:23:38 -0500

    i dont know anymore

---
+++ src/components/Index.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+
+import '../static/styles/index.css';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>React simple starter</div>
+    );
+  }
+}
